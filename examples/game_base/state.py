04820a078da04ce24d35e0dd4910f64b20ff080b
Alexander Niedner <acniedner@gmail.com>
2023-04-05 22:23:31 -0700

    moves implemented print_board into correct package and adds interface function for it

--- examples/game_base/state.py
+++ examples/game_base/state.py
@@ -53,12 +53,4 @@ class State:
                 yield position
 
     def print_board(self) -> None:
-        for i in range(self.board.shape[0]):
-            print("-" * (4 * self.board.shape[1] + 1))
-            out = "| "
-            for j in range(self.board.shape[1]):
-                token: Symbol | str | None = self.board[i, j] or " "
-                out += str(token) + " | "
-            print(out)
-
-        print("-" * (4 * self.board.shape[1] + 1))
+        raise NotImplementedError
--- examples/tic_tac_toe/state.py
+++ examples/tic_tac_toe/state.py
@@ -5,8 +5,17 @@ from typing import TYPE_CHECKING
 from examples.game_base import State
 
 if TYPE_CHECKING:
-    pass
+    from .symbol import TicTacToeSymbol
 
 
 class TicTacToeState(State):
-    pass
+    def print_board(self) -> None:
+        for i in range(self.board.shape[0]):
+            print("-" * (4 * self.board.shape[1] + 1))
+            out = "| "
+            for j in range(self.board.shape[1]):
+                token: TicTacToeSymbol | str | None = self.board[i, j] or " "
+                out += str(token) + " | "
+            print(out)
+
+        print("-" * (4 * self.board.shape[1] + 1))
