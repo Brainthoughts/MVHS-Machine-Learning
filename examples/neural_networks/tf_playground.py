9406950d2de274e4ed940e470b0e5584f1df3b61
Alexander Niedner <acniedner@gmail.com>
2023-04-27 10:49:35 -0700

    adds ability to toggle on/off training data and switch between gradient and point representation of output

--- examples/neural_networks/tf_playground.py
+++ examples/neural_networks/tf_playground.py
@@ -39,7 +39,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 
-def visualize_decision_boundary(model, X, Y):
+def visualize_decision_boundary_gradient(model, X, Y, show_training_data=True):
     # Generate a grid of points from -5 to 5 with a step of 0.1
     xx, yy = np.meshgrid(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1))
     # Flatten the grids into a 1D array
@@ -59,21 +59,50 @@ def visualize_decision_boundary(model, X, Y):
 
     # Plot the decision boundary
     plt.contourf(xx, yy, zz, cmap=colormaps['RdBu'], alpha=0.8)
-    plt.scatter(X[Y == 0][:, 0], X[Y == 0][:, 1], c='red', marker='o', label='Class 0')
-    plt.scatter(X[Y == 1][:, 0], X[Y == 1][:, 1], c='blue', marker='s', label='Class 1')
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
+    if show_training_data:
+        plt.scatter(X[Y == 0][:, 0], X[Y == 0][:, 1], c='red', marker='o', label='Class 0')
+        plt.scatter(X[Y == 1][:, 0], X[Y == 1][:, 1], c='blue', marker='s', label='Class 1')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
     plt.title('Decision Boundary')
-    plt.legend()
     plt.show()
 
+def visualize_decision_boundary_points(model, X, Y, point_spacing = .5, show_training_data=True):
+    # Generate a grid of points from -5 to 5 with a step of 0.1
+    xx, yy = np.meshgrid(np.arange(-5, 5, point_spacing), np.arange(-5, 5, point_spacing))
+    # Flatten the grids into a 1D array
+    xx_flattened = xx.ravel()
+    yy_flattened = yy.ravel()
+    # Concatenate the flattened grids to create a matrix of input points
+    input_points = np.vstack((xx_flattened, yy_flattened)).T
+    # Convert the input points to a torch tensor
+    input_points = torch.tensor(input_points, dtype=torch.float32)
+    # Get the predictions for the input points from the trained model
+    with torch.no_grad():
+        predictions = model(input_points)
+    # Convert the predictions to a numpy array
+    predictions = predictions.numpy()
+    # Reshape the predictions to match the shape of the original grids
+    zz = predictions.reshape(xx.shape)
+
+    # Plot the decision boundary
+    plt.scatter(xx, yy, c=zz, cmap=colormaps['RdBu'], alpha=0.8)
+    if show_training_data:
+        plt.scatter(X[Y == 0][:, 0], X[Y == 0][:, 1], c='red', marker='o', label='Class 0')
+        plt.scatter(X[Y == 1][:, 0], X[Y == 1][:, 1], c='blue', marker='s', label='Class 1')
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.legend()
+    plt.title('Decision Boundary')
+    plt.show()
 
 # Training loop
 num_epochs = 500
 for epoch in range(num_epochs):
     epoch_loss = 0.
-    if epoch % math.ceil(num_epochs/30) == 0:  # max of 30 plots
-        visualize_decision_boundary(net, dataset.points, dataset.labels)
+    if epoch % math.ceil(num_epochs/(30-1)) == 0:  # max of 30 plots, 29 while training and 1 after
+        visualize_decision_boundary_points(net, dataset.points, dataset.labels)
     for i, (inputs, targets) in enumerate(dataloader):
         # Zero the gradients
         optimizer.zero_grad()
@@ -94,4 +123,4 @@ for epoch in range(num_epochs):
 
 print('Training finished.')
 
-visualize_decision_boundary(net, dataset.points, dataset.labels)
+visualize_decision_boundary_points(net, dataset.points, dataset.labels,  show_training_data=False)
